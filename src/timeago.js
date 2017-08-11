251658a837f8d67d27d349b795cf482cf424805e
kira san <ikbelnet@gmail.com>
2017-08-11 22:42:00 +0100

    First commit

---
+++ src/timeago.js
@@ -0,0 +1,41 @@
+import moment from 'moment'
+
+export default {
+
+  template: `<div class="time-ago">{{elapsed}}</div>`,
+
+  props: {
+    time: {
+      type: Number,
+      default: 0,
+    }
+  },
+
+  data() {
+    return {
+      now: moment().unix(),
+      timerID: 0,
+    }
+  },
+
+  computed: {
+    elapsed() {
+      var diff = (this.now - this.time) * 1000
+      return moment.duration(diff).humanize() + ' ago'
+    },
+  },
+
+  methods: {
+    update() {
+      this.now = moment().unix()
+    }
+  },
+
+  mounted() {
+    this.timerID = setInterval(this.update, 1000)
+  },
+
+  beforeDestroy() {
+    clearInterval(this.timerID)
+  }
+}
